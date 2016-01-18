cc651f58a653dd4e0f697694be01a8c449ea4388
gs298 <shrey.om11@gmail.com>
2016-01-17 17:40:49 -0800

    commit

--- ChartApplication/WebContent/chart.js
+++ ChartApplication/WebContent/chart.js
@@ -63,7 +63,7 @@ angular.module('AngularChart', []).directive('chart', function () {
                 },
                 series:[
                     {
-                        name:'India',
+                        name:'America',
                         data:[7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
                     }
                 ]
